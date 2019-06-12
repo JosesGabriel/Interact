560ac1670a19e51f7bf140311d04bc8e675c80c8
Emmanuel Tierra <emman.tierra@gmail.com>
2019-06-12 20:24:55 +0800

    Set unauthorized route

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@
 
 Route::get('/', function () {
     return [
-        'status' => 301,
-        'title' => 'Unauthorized access.',
+        'status' => 401,
+        'message' => 'Unauthorized access.',
     ];
-});
+})->name('unauthorized');
