9940779ec196ff3ff2642d28ad18fcd0f862f954
Emmanuel Tierra <emman.tierra@gmail.com>
2019-07-17 14:27:44 +0800

    Create Post model and config

---
+++ app/Data/Models/Posts/Post.php
@@ -0,0 +1,69 @@
+<?php
+
+
+namespace App\Data\Models\Posts;
+
+use App\Data\Models\BaseModel;
+use Illuminate\Support\Str;
+
+/**
+ * Class Post
+ *
+ * @package App\Data\Models\Posts
+ */
+class Post extends BaseModel
+{
+    //region Configs
+    protected $fillable = [
+        'user_id',
+        'content',
+        'status',
+        'visibility',
+    ];
+
+    protected $hidden = [
+        'id',
+    ];
+
+    protected $rules = [
+        'user_id' => 'sometimes|required',
+        'content' => 'sometimes|required',
+        'status' => 'sometimes|required',
+        'visibility' => 'sometimes|required',
+    ];
+    //endregion Configs
+
+    //region Finders
+    /**
+     * @param string $uuid
+     * @return mixed
+     */
+    public static function findByUUID($uuid)
+    {
+        return static::byUUID($uuid)->first();
+    }
+    //endregion Finders
+
+    //region Scopes
+    /**
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param string $uuid
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeByUUID($query, $uuid)
+    {
+        return $query->where('uuid', $uuid);
+    }
+    //endregion Scopes
+
+    //region Events
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($user) {
+            $user->uuid = Str::uuid()->toString();
+        });
+    }
+    //endregion Events
+}
