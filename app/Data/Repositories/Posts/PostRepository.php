27b3b3b94ea98acc5f9d3ec0cdb47ef556abdd9f
Emmanuel Tierra <emman.tierra@gmail.com>
2019-07-18 16:25:12 +0800

    Implement update in PostRepository

--- app/Data/Repositories/Posts/PostRepository.php
+++ app/Data/Repositories/Posts/PostRepository.php
@@ -146,10 +146,77 @@ class PostRepository extends BaseRepository
     /**
      * @param $id
      * @param array $data
-     * @return mixed
+     * @return PostRepository
      */
     public function update($id, array $data)
     {
-        // TODO: Implement update() method.
+        $post_id = null;
+        $user_id = null;
+
+        //region Data validation
+        if (!isset($id) ||
+            !is_numeric($id)) {
+            return $this->setResponse([
+                'status' => 417,
+                'message' => 'The post id is not set or invalid.',
+            ]);
+        }
+
+        if (!isset($data['user_id']) ||
+            trim($data['user_id']) == '') {
+            return $this->setResponse([
+                'status' => 417,
+                'message' => 'The user id is not set or invalid.',
+            ]);
+        }
+
+        if (isset($data['id'])) {
+            unset($data['id']);
+        }
+        //endregion Data validation
+
+        $user_id = $data['user_id'];
+        unset($data['user_id']);
+
+        //region Existence check
+        $post = $this->fetch($id);
+
+        if ($post->isError()) {
+            return $post;
+        }
+        //endregion Existence check
+
+        $post = $post->getDataByKey('post');
+
+        //region Authorization check
+        // if this post does not belong to the user
+        if ($user_id !== $post->user_id) {
+            return $this->setResponse([
+                'status' => 403,
+                'message' => 'The user is unable to update this post.',
+            ]);
+        }
+        //endregion Authorization check
+
+        //region Data update
+        if (!$post->save($data)) {
+            $errors = $post->getErrors();
+            return $this->setResponse([
+                'status' => 500,
+                'message' => 'An error has occurred while updating the post.',
+                'meta' => [
+                    'errors' => $errors,
+                ],
+            ]);
+        }
+        //endregion Data update
+
+        return $this->setResponse([
+            'status' => 200,
+            'message' => 'Successfully updated the post.',
+            'data' => [
+                'post' => $post,
+            ],
+        ]);
     }
 }
