628ae86578280ebcf84a93d3085ef4b047ab7620
Emmanuel Tierra <emman.tierra@gmail.com>
2019-07-23 14:32:01 +0800

    Create sentiment model config

---
+++ config/arbitrage/sentiments/model.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'sentimentable_type' => [
+        'comment' => [
+            'label' => 'comment',
+            'value' => 'App\Data\Models\Posts\Comments\Comment',
+        ],
+        'post' => [
+            'label' => 'post',
+            'value' => 'App\Data\Models\Posts\Post',
+        ],
+    ],
+    'type' => [
+        'bear' => [
+            'label' => 'bear',
+            'value' => 'bear',
+        ],
+        'bull' => [
+            'label' => 'bull',
+            'value' => 'bull',
+        ],
+    ],
+];
