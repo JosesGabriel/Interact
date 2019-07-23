0024fa37419dc8611d0e01686618630cdf059688
Emmanuel Tierra <emman.tierra@gmail.com>
2019-07-23 14:31:13 +0800

    Create Sentiment migration and model

---
+++ database/migrations/2019_07_22_062018_create_sentiments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSentimentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sentiments', function (Blueprint $table) {
+            $table->bigInteger('id')->primary();
+            $table->bigInteger('sentimentable_id')->index();
+            $table->string('sentimentable_type');
+            $table->uuid('user_id');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sentiments');
+    }
+}
