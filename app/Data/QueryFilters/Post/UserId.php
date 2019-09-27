4ae020dbc760dc8cfe05f87ebed5de0a2f9b0d83
Emmanuel Tierra <emman.tierra@gmail.com>
2019-09-27 14:50:01 +0800

    Add filter for Post user_id

---
+++ app/Data/QueryFilters/Post/UserId.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Data\QueryFilters\Post;
+
+use Arbitrage\Abstracts\Pipelines\Eloquent\Filter;
+use Illuminate\Database\Eloquent\Builder;
+
+/**
+ * Class UserId
+ *
+ * @package App\Data\QueryFilters\Post
+ */
+class UserId extends Filter
+{
+    /**
+     * @param Builder $builder
+     * @return Builder
+     */
+    protected function applyFilter(Builder $builder): Builder
+    {
+        return $builder->where('user_id', request('user_id'));
+    }
+}
