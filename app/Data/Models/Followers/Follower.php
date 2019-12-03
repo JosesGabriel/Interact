10769c8a93a5a706f2ff20fa121716be0ec2a944
Emmanuel Tierra <emman.tierra@gmail.com>
2019-12-03 13:55:03 +0800

    Add isFollower, isFollowing, and profile scopes to Follower

--- app/Data/Models/Followers/Follower.php
+++ app/Data/Models/Followers/Follower.php
@@ -35,4 +35,75 @@ class Follower extends BaseModel
         'follower_id' => 'sometimes|required|uuid',
     ];
     //endregion Configs
+
+    //region Scopes
+    /**
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param $user_id
+     * @param $follower_id
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeIsFollower($query, $user_id, $follower_id)
+    {
+        return $query->addSelect([
+            'is_follower' => function ($subquery) use ($user_id, $follower_id) {
+                $subquery
+                    ->selectRaw('count(*)')
+                    ->from('followers')
+                    ->where('user_id', $user_id)
+                    ->where('follower_id', $follower_id)
+                    ->first();
+            },
+        ]);
+    }
+
+    /**
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param $follow_id
+     * @param $follower_id
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeIsFollowing($query, $follow_id, $follower_id)
+    {
+        return $query->addSelect([
+            'is_following' => function ($subquery) use ($follow_id, $follower_id) {
+                $subquery
+                    ->selectRaw('count(*)')
+                    ->from('followers')
+                    ->where('user_id', $follow_id)
+                    ->where('follower_id', $follower_id)
+                    ->first();
+            },
+        ]);
+    }
+
+    /**
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param $user_id
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeProfile($query, $user_id)
+    {
+        return $query->addSelect([
+            'followers' => function ($subquery) use ($user_id) {
+                $subquery
+                    ->selectRaw('count(*)')
+                    ->from('followers')
+                    ->where('user_id', $user_id);
+            },
+            'following' => function ($subquery) use ($user_id) {
+                $subquery
+                    ->selectRaw('count(*)')
+                    ->from('followers')
+                    ->where('follower_id', $user_id);
+            },
+            'posts' => function ($subquery) use ($user_id) {
+                $subquery
+                    ->selectRaw('count(*)')
+                    ->from('posts')
+                    ->where('user_id', $user_id);
+            },
+        ]);
+    }
+    //endregion Scopes
 }
