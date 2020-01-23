a405695253fe823812647958b67400f73bda9362
Emmanuel Tierra <emman.tierra@gmail.com>
2020-01-23 20:46:59 +0800

    Add new post listener

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -12,7 +12,9 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-
+        \App\Events\Posts\UserPostedEvent::class => [
+            \App\Listeners\Posts\NewPostNotification::class,
+        ],
     ];
 
     /**
