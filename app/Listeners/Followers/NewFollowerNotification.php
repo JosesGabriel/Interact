69a75e23dd7317950bdf1b5be1a8c19cf25d906b
Emmanuel Tierra <emman.tierra@gmail.com>
2020-01-24 15:07:39 +0800

    Add listener for follow notification

---
+++ app/Listeners/Followers/NewFollowerNotification.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Listeners\Followers;
+
+use App\Events\Followers\UserFollowedEvent;
+use App\Jobs\SendWebNotification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+/**
+ * Class NewFollowerNotification
+ *
+ * @package App\Listeners\Followers
+ */
+class NewFollowerNotification implements ShouldQueue
+{
+    use InteractsWithQueue;
+
+    /**
+     * @var SendWebNotification
+     */
+    private $sendWebNotification;
+
+    /**
+     * Create the event listener.
+     *
+     * @param SendWebNotification $sendWebNotification
+     */
+    public function __construct(SendWebNotification $sendWebNotification)
+    {
+        $this->sendWebNotification = $sendWebNotification;
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  UserFollowedEvent  $event
+     * @return void
+     */
+    public function handle(UserFollowedEvent $event)
+    {
+        $follower = $event->follower;
+
+        $this->sendWebNotification::dispatch([
+            'message' => 'A user followed you.',
+            'data' => [
+                'follower' => [
+                    'id' => $follower->follower_id,
+                ],
+            ],
+        ], 'social.user.follow', $follower->user_id);
+    }
+}
