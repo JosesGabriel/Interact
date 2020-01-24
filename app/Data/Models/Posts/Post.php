bbfa9a9da319ddad110719c7b6f50d973ae5a92a
Emmanuel Tierra <emman.tierra@gmail.com>
2020-01-24 11:28:59 +0800

    Add tagged Stocks relation to Post

--- app/Data/Models/Posts/Post.php
+++ app/Data/Models/Posts/Post.php
@@ -81,6 +81,11 @@ class Post extends BaseModel
         return $this->morphMany(config('arbitrage.models_map.tags.tag'), 'taggable');
     }
 
+    public function taggedStocks()
+    {
+        return $this->tags()->where('tag_type', config('arbitrage.tags.model.tag_type.stock.value'));
+    }
+
     public function taggedUsers()
     {
         return $this->tags()->where('tag_type', config('arbitrage.tags.model.tag_type.user.value'));
