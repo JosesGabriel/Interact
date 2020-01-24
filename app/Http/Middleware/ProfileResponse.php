3e24c160799b53d5987c90c9f8cce63d6a6b3d92
Emmanuel Tierra <emman.tierra@gmail.com>
2020-01-24 15:21:26 +0800

    Update middlware for Laravel Debugbar

--- app/Http/Middleware/ProfileResponse.php
+++ app/Http/Middleware/ProfileResponse.php
@@ -2,31 +2,8 @@
 
 namespace App\Http\Middleware;
 
-use Closure;
+use Arbitrage\Http\Middleware\LaravelDebugBar;
 
-class ProfileResponse
+class ProfileResponse extends LaravelDebugBar
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle($request, Closure $next)
-    {
-        $response = $next($request);
-
-        if (
-            app()->bound('debugbar') &&
-            app('debugbar')->isEnabled()
-        ){
-            $original = $response->getOriginalContent();
-            $original['_debugbar'] = app('debugbar')->getData();
-
-            $response->setContent(json_encode($original));
-        }
-
-        return $response;
-    }
 }
