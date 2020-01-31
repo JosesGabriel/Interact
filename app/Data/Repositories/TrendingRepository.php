fe14d3665b896a6b98c37ae868208cc4550a811c
Kenneth Arphel Balboa <e3arphie@gmail.com>
2020-01-31 18:11:59 +0800

    adctivate the limit on query

--- app/Data/Repositories/TrendingRepository.php
+++ app/Data/Repositories/TrendingRepository.php
@@ -185,8 +185,8 @@ class TrendingRepository extends BaseRepository
     public function get_users($data)
     {
         $trending_days = 90;
-        // $limit = (isset($data['count']) ? $data['count'] : 5);
-        $limit = 20;
+        $limit = (isset($data['count']) ? $data['count'] : 5);
+        // $limit = 10;
         $post_stocks = $this->post_model->where('created_at', '>=', Carbon::now()->subDays($trending_days)->toDateTimeString())->get()->toArray();
 
         if(empty($post_stocks)){
